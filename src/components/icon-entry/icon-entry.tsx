2bc1ea3068901835b118ab30db1c2cace9da681e
Shaheer <shaheer@deriv.com>
2023-11-09 18:23:04 +0400

    style: :lipstick: updates style

--- src/components/icon-entry/icon-entry.tsx
+++ src/components/icon-entry/icon-entry.tsx
@@ -12,10 +12,13 @@ const IconEntry = ({ iconName, icon }: TIconEntryProps) => {
   const Component = icon as unknown as JSX.Element;
   return (
     <div
+      onKeyDown={undefined}
       onClick={() => setSelected((selected) => !selected)}
       className={classNames(
-        "flex cursor-pointer flex-col items-center justify-between rounded-xl border-2 border-transparent p-4 shadow-md hover:bg-slate-100",
-        selected ? "border-rose-500" : "hover:border-slate-400",
+        "flex cursor-pointer flex-col items-center justify-between rounded-xl border-2 p-4 shadow-md",
+        selected
+          ? "border-rose-500 bg-rose-50"
+          : "border-transparent hover:border-slate-400 hover:bg-slate-50",
       )}
     >
       <span />
